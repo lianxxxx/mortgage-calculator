312e8e06ff5379097fa5c3ed91437d1fb979a253
lianxxxx <leyannemaemayo21@gmail.com>
2025-12-27 15:32:43 +0800

    Finish layout for results display section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import InputField from "./components/MortgageForm/InputField";
 import Placeholder from "./components/Results/Placeholder";
+import ResultDisplay from "./components/Results/ResultDisplay";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <h2 className="underline clear-text">Clear All</h2>
           <InputField />
           <Placeholder />
+          <ResultDisplay />
         </section>
       </main>
     </>
--- src/components/Results/ResultDisplay.jsx
+++ src/components/Results/ResultDisplay.jsx
@@ -1,5 +1,22 @@
 function ResultDisplay() {
-  return <div></div>;
+  return (
+    <div className="results-display text-white">
+      <h1 className="result-text">Your results</h1>
+      <p className="result-description">
+        Your results are shown below based on the information you provided. To
+        adjust the results, edit the form and click "calculate repayments"
+        again.
+      </p>
+      <div style={{color:"var(--slate-normal)"}} className="result-box">
+      
+        <p>Your monthly repayments</p>
+        <h1 className="font-bold text-3xl amount-monthly-repayments" style={{color:"var(--lime)"}}>$0</h1>
+        <hr />
+        <p>Total you'll repay over the term</p>
+        <h3 className="font-bold amount-monthly-repay-term" style={{color:"var(--white)"}}>£0</h3>
+      </div>
+    </div>
+  );
 }
 
 export default ResultDisplay;
